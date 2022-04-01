ae4600b622676d9f316a389641752696d1fa68df
nurekeshka <crystalmaiden666123@gmail.com>
2022-04-01 07:36:15 +0600

    feat: timer update

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 const Cron = require("node-cron");
+var childProcess = require('child_process');
+
+Cron.schedule("* * * * *", async () => {
+    const output = childProcess.execSync("npm run upload").toString();
+    console.log(output);
+});
